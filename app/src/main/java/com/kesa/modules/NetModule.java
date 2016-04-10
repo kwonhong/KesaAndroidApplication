6bfc4c848de4a1adcdc64fddc779c19277ae39b8
James Kwon <hongilkwon316@gmail.com>
2016-04-10 15:45:10 -0400

    Integrating Dagger 2 dependency injection

---
+++ app/src/main/java/com/kesa/modules/NetModule.java
@@ -0,0 +1,13 @@
+package com.kesa.modules;
+
+import dagger.Module;
+
+/**
+ * A module containing the provide methods for dependency graph.
+ *
+ * @author hongil@
+ */
+@Module
+public class NetModule {
+
+}
