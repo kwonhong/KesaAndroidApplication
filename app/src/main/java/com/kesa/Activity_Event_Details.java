017e2eba23ea22a4a7ac6c104a2899a61728e599
Taihyun Lee <th9061@hotmail.com>
2016-06-10 01:27:50 -0400

    added the event detail

---
+++ app/src/main/java/com/kesa/Activity_Event_Details.java
@@ -0,0 +1,13 @@
+package com.kesa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Event_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity__event__details);
+    }
+}
